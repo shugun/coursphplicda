997c2152017c3fe0000e30ae6b30011dd24d3e75
Pyxicom_hicham <hamdan@pyxicom.com>
2017-02-26 10:21:09 +0000

    changement de la config

--- config/config.php
+++ config/config.php
@@ -2,11 +2,16 @@
 error_reporting(E_ALL);
 //session_start();
 define('T_ETUDIANTS','etudiants');
+define('MYSQL_HOST','localhost');
+define('MYSQL_DB','licda');
+define('MYSQL_USER','root');
+define('MYSQL_PASSWORD','root');
 $uploads_dir = './uploads';
 global $conn;
 function connexionBaseDeDonne(){
 	global $conn;
-	$conn = new PDO('mysql:host=localhost;dbname=licda', 'root', 'root');
+	//$conn = new PDO('mysql:host=localhost;dbname=licda', 'root', 'root');
+	$conn = new PDO('mysql:host='.MYSQL_HOST.';dbname='.MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD);
 	//return $conn;retrieve_password():
 }
 function supprimer($table,$id){
